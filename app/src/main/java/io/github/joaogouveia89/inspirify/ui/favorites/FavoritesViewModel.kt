f81c2d98738e2a143def1575ae3cee525c382249
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-02-16 09:38:55 +0000

    renamed notifications to Favorites

--- app/src/main/java/io/github/joaogouveia89/inspirify/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/io/github/joaogouveia89/inspirify/ui/favorites/FavoritesViewModel.kt
@@ -1,10 +1,10 @@
-package io.github.joaogouveia89.inspirify.ui.notifications
+package io.github.joaogouveia89.inspirify.ui.favorites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FavoritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
