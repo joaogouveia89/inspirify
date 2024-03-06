76ef01abc348fa5a36b03470f76aae640b7da745
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-06 20:48:22 +0000

    add unit test to checkFavoriteStateUseCase in viewmodel

--- app/src/test/java/io/github/joaogouveia89/inspirify/viewmodels/quoteShow/QuoteShowViewModelTest.kt
+++ app/src/test/java/io/github/joaogouveia89/inspirify/viewmodels/quoteShow/QuoteShowViewModelTest.kt
@@ -3,12 +3,14 @@ package io.github.joaogouveia89.inspirify.viewmodels.quoteShow
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import io.github.joaogouveia89.inspirify.InspirifyViewModelUnitTest
+import io.github.joaogouveia89.inspirify.data.DataRequest
 import io.github.joaogouveia89.inspirify.data.local.entities.Favorite
 import io.github.joaogouveia89.inspirify.di.InspirifyComponent
 import io.github.joaogouveia89.inspirify.ui.quoteShow.Quote
 import io.github.joaogouveia89.inspirify.ui.quoteShow.QuoteShowViewModel
 import io.github.joaogouveia89.inspirify.ui.quoteShow.QuoteShowViewModelFactory
 import io.github.joaogouveia89.inspirify.ui.quoteShow.asFavorite
+import io.github.joaogouveia89.inspirify.ui.quoteShow.useCases.CheckFavoriteStateUseCase
 import io.github.joaogouveia89.inspirify.ui.quoteShow.useCases.QuoteAddToFavoriteUseCase
 import io.github.joaogouveia89.inspirify.ui.quoteShow.useCases.QuoteShowUseCase
 import io.mockk.Runs
@@ -18,6 +20,9 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertNull
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
@@ -30,13 +35,13 @@ class QuoteShowViewModelTest : InspirifyViewModelUnitTest() {
     // Mock dependencies
     private val quoteShowUseCase: QuoteShowUseCase = mockk()
     private val quoteAddToFavoriteUseCase: QuoteAddToFavoriteUseCase = mockk()
+    private val checkFavoriteStateUseCase: CheckFavoriteStateUseCase = mockk()
 
     // Lateinit ViewModel and ViewModelFactory
     private lateinit var viewModel: QuoteShowViewModel
     private lateinit var viewModelFactory: QuoteShowViewModelFactory
 
     private val quote: Quote = mockk(relaxed = true)
-    private val favorited: Favorite = quote.asFavorite()
 
     @Before
     fun setup() {
@@ -46,14 +51,19 @@ class QuoteShowViewModelTest : InspirifyViewModelUnitTest() {
             val viewModel: QuoteShowViewModel = firstArg()
             viewModel.quoteShowUseCase = quoteShowUseCase
             viewModel.quoteAddToFavoriteUseCase = quoteAddToFavoriteUseCase
+            viewModel.checkFavoriteStateUseCase = checkFavoriteStateUseCase // Injecting checkFavoriteStateUseCaseMock
         }
 
         // Mock the behavior of getDataRequest() function in QuoteShowUseCase
         every { quoteShowUseCase.dataRequest } returns MutableLiveData()
         every { quoteAddToFavoriteUseCase.dataRequest } returns MutableLiveData()
+        every { checkFavoriteStateUseCase.dataRequest } returns MutableLiveData()
 
         // Pass the mocked component to your ViewModelFactory
         viewModelFactory = QuoteShowViewModelFactory(inspirifyComponent)
+
+        // Create the ViewModel instance
+        viewModel = viewModelFactory.create(QuoteShowViewModel::class.java)
     }
 
     @After
@@ -67,9 +77,6 @@ class QuoteShowViewModelTest : InspirifyViewModelUnitTest() {
         // Given - Nothing to do here
         coEvery { quoteShowUseCase.execute() } just Runs
 
-        // Create your ViewModel using the mocked ViewModelFactory
-        viewModel = viewModelFactory.create(QuoteShowViewModel::class.java)
-
         // When
         viewModel.inputs.requestNewData.postValue(Unit)
 
@@ -79,13 +86,9 @@ class QuoteShowViewModelTest : InspirifyViewModelUnitTest() {
 
     @Test
     fun `onFavoriteClick should execute quoteFavoriteUseCase`() = runTest {
-        // Create your ViewModel using the mocked ViewModelFactory
-        viewModel = viewModelFactory.create(QuoteShowViewModel::class.java)
 
         // Mock the behavior of currentQuote to return your quote
-        val currentQuoteObserver = Observer<Quote> { updatedQuote ->
-            assertEquals(quote, updatedQuote)
-            // Verify that quoteAddToFavoriteUseCase.execute was called
+        val currentQuoteObserver = Observer<Quote> { _ ->
             coVerify { quoteAddToFavoriteUseCase.execute(quote) }
         }
 
@@ -94,6 +97,22 @@ class QuoteShowViewModelTest : InspirifyViewModelUnitTest() {
 
         // Trigger the onFavoriteClick action
         viewModel.inputs.onFavoriteClick.postValue(Unit)
+
+        // Remove the observer to avoid leaks
+        viewModel.currentQuote.removeObserver(currentQuoteObserver)
+    }
+
+    @Test
+    fun `checkFavoriteState should execute checkFavoriteStateUseCase`() = runTest {
+        // Mock the behavior of currentQuote to return your quote
+        val currentQuoteObserver = Observer<Quote> { _ ->
+            coVerify { checkFavoriteStateUseCase.execute(quote) }
+        }
+
+        viewModel.outputs.currentQuote.observeForever(currentQuoteObserver)
+
+        // When - Trigger checkFavoriteState
+        viewModel.inputs.checkFavoriteState.postValue(Unit)
 
         // Remove the observer to avoid leaks
         viewModel.currentQuote.removeObserver(currentQuoteObserver)
